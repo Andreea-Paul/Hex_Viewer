a97f988bc6e8ed359f25601896f19011488451a2
Roxana Paul <andreeapaul@protonmail.com>
2022-11-23 13:47:22 +0200

    Add project files.

---
+++ Hex_Viewer/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hex_Viewer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\carti\progit.pdf";
+
+            using (FileStream file = File.OpenRead(path))
+            {
+                byte[] bytes = new byte[16];
+                int batch,counter;
+                counter= 0;
+
+                do
+                {
+                    string line=string.Empty;
+                    batch = file.Read(bytes, 0, 16);
+
+                    counter++;
+                    string counter_hex = Convert.ToString(counter, 16);
+                    while (counter_hex.Length < 6)
+                    {
+                        counter_hex = "0" + counter_hex;
+                    }
+
+                    string hexString = BitConverter.ToString(bytes).Replace("-", " ");
+                    
+                    for (int i = 0; i < bytes.Length; i++)
+                    {
+                        if (bytes[i] < 33 || bytes[i] > 126 && bytes[i] < 159)
+                        {
+                            line += ".";
+                        }
+                        else
+                        {
+                            line += (char)bytes[i];
+                        }
+                    }
+                    
+                    Console.WriteLine($"{counter_hex}:  {hexString}  {line}");
+                }
+                while (batch == 16);
+            }
+        }
+    }
+}
+        
+    
+
